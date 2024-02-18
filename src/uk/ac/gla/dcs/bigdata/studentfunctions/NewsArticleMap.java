1bcb5d570682cc80c1eafba5aa99bfc2ed157115
Elvis LIN <lzh000307@126.com>
2024-02-18 01:09:11 +0000

    rec

--- src/uk/ac/gla/dcs/bigdata/studentfunctions/NewsArticleMap.java
+++ src/uk/ac/gla/dcs/bigdata/studentfunctions/NewsArticleMap.java
@@ -12,14 +12,14 @@ import java.util.List;
 public class NewsArticleMap implements MapFunction<NewsArticle, NewsArticleProcessed> {
 
     private transient TextPreProcessor newsProcessor;
+    private List<String> contentsProcessed;
 
-
-    public NewsArticleMap() {
-        newsProcessor = new TextPreProcessor();
-    }
     @Override
     public NewsArticleProcessed call(NewsArticle value) throws Exception {
-        if(newsProcessor == null) newsProcessor = new TextPreProcessor();
+        if(newsProcessor == null) {
+            newsProcessor = new TextPreProcessor();
+        }
+        contentsProcessed = new ArrayList<String>();
         String id = value.getId();
         String title = value.getTitle();
 //        String articleURL = value.getArticle_url();
@@ -32,16 +32,25 @@ public class NewsArticleMap implements MapFunction<NewsArticle, NewsArticleProce
 
         int articleLength = 0;
         int paragraphNum = 0;
-        List<String> contentsProcessed = new ArrayList<String>();
-        for (int i = 0; i < contents.size();i++) {
-        	String ret = String.join(" ", newsProcessor.process(contents.get(i).getContent()));
-        	contentsProcessed.add(ret);
+        for (ContentItem content : contents) {
+            // if not null, get the subtype
+            if(content.getSubtype() != null && content.getSubtype().equals("paragraph")) {
+                // check for and handle missing or null fields in the data.
+                if(content.getContent().isBlank()) {
+                    System.out.println();
+                    continue;
+                }
+                // only get the first 5 paragraphs
+                if(++paragraphNum == 5)
+                    break;
+//                System.out.println("Content: " + content.getContent());
+                contentsProcessed.addAll(newsProcessor.process(content.getContent()));
+            }
         }
-
-        articleLength = contentsProcessed.size();
+        articleLength += contentsProcessed.size();
         //for the title:
         List<String> titleProcessed = newsProcessor.process(title);
-        articleLength = titleProcessed.size();
+        articleLength += titleProcessed.size();
         return new NewsArticleProcessed(id, titleProcessed, contentsProcessed, articleLength);
     }
 }
